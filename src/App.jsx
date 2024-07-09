0d52c0f9b4bbf9e67dba3dbc217c6e732466c657
Codeuzz <belinda.zilevu@gmail.com>
2024-07-09 17:03:42 +0200

    moved state from App to AddProduct

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,13 @@ import ProductFilter from './Components/ProductFilter';
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [prix, setPrix] = useState(0);
-  const [nom, setNom] = useState('');
-  const [categorie, setCategorie] = useState('')
+  
   const [filtre, setFiltre] = useState('tous');
   const [filtrePrix, setFiltrePrix] = useState('croissant');
 
-
-  const handleChange = (e) => {
-    if(e.target.name === "nom") {
-      setNom(e.target.value)
-    } else if( e.target.name === "categorie") {
-      setCategorie(e.target.value)
-    } else if(e.target.name === "prix") {
-      setPrix(e.target.value)
-    } 
-  }
-
-  const saveInfos = (e) => {
-    e.preventDefault();
-    const newProduct = {"nom": nom, "prix": prix, "categorie": categorie}
+  const saveInfos = (newProduct) => {
     setProducts([...products, newProduct]);
-    
-    resetState()
   }
-
-  const resetState = () => {
-    setNom('');
-    setPrix(0);
-    setCategorie('')
-  };
 
   const handleFilterChange = (e) => {
     setFiltre(e.target.value)
@@ -45,8 +22,6 @@ function App() {
     setFiltrePrix(e.target.value)
   }
 
-
-
   useEffect(() => {
     console.log(products);
     console.log("filtre :", filtre)
@@ -55,7 +30,7 @@ function App() {
 
   return (
     <>
-      <AddProduct nom={nom} prix={prix} categorie={categorie} handleChange={handleChange} saveInfos={saveInfos} />
+      <AddProduct saveInfos={saveInfos} />
       <ProductFilter  filtrePrix={filtrePrix} handleFilterPrixChange={handleFilterPrixChange} handleFilterChange={handleFilterChange} filtre={filtre} />
       <ProductList setProducts={setProducts} filtrePrix={filtrePrix} filtre={filtre} products={products} />
 
--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -1,6 +1,37 @@
-function AddProduct({handleChange, saveInfos, categorie, prix, nom}) {
+import { useState } from 'react'
+
+
+function AddProduct({saveInfos}) {
+    const [prix, setPrix] = useState(0);
+    const [nom, setNom] = useState('');
+    const [categorie, setCategorie] = useState('')
+    
+    const handleChange = (e) => {
+        if (e.target.name === "nom") {
+          setNom(e.target.value);
+        } else if (e.target.name === "categorie") {
+          setCategorie(e.target.value);
+        } else if (e.target.name === "prix") {
+          setPrix(e.target.value);
+        }
+      };
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const newProduct = { nom, prix, categorie };
+        saveInfos(newProduct);
+        resetState();
+      }; 
+
+      const resetState = () => {
+        setNom('');
+        setPrix(0);
+        setCategorie('');
+      };
+
+
     return (
-        <form onSubmit={saveInfos}>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="nom-produit">Ajouter un produit:</label>
             <input value={nom} onChange={handleChange} name="nom" id="nom-produit" type="text" placeholder="Nom du produit" />
             <select value={categorie} onChange={handleChange} name="categorie" >
@@ -16,3 +47,6 @@ function AddProduct({handleChange, saveInfos, categorie, prix, nom}) {
 }
 
 export default AddProduct
+
+// mettre a jour state ici
+// renommer save infos avec on ok
